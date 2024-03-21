bfba49e60fd24eeb9d212fb9f62b2a9f523b4d89
Andreas Neudecker <andreas.neudecker@inwt-statistics.de>
2024-03-21 14:32:49 +0100

    Add parameter to format cells

---
+++ inst/appFormat/ui.R
@@ -0,0 +1,27 @@
+library(shinyMatrix)
+
+m <- diag(5)
+# colnames(m) <- 1:5
+# rownames(m) <- letters[1:5]
+
+shiny::tagList(
+  shiny::fluidPage(
+    shiny::titlePanel("Demonstration Matrix Input Field"),
+    shiny::fluidRow(
+      column(6, matrixInput(
+        inputId = "matrix",
+        label = "Default matrix",
+        value = m,
+        class = "numeric",
+        cols = list(
+          names = TRUE
+        ),
+        rows = list(
+          names = TRUE
+        ),
+        formatCell = ".2f"
+      )),
+      column(6, tableOutput("table"))
+    )
+  )
+)
